0ad5163ebc64b43e7b9f64740240da60433207ae
silah <silakosy@gmail.com>
2023-04-06 22:34:25 +0300

    first commit

---
+++ src/src/Providers/autogenerateModelProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace silah\autogenerateModel\Providers;
+
+use Illuminate\Support\ServiceProvider;
+class autogenerateModelProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
